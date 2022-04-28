af669bf9473e7dfc2296af72d4ab0d6676d19d4f
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-28 14:22:36 -0300

    estrutura do reactjs

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { App } from './App'
+import { render } from 'react-dom';
+import { App } from './App';
 
-function Home() {
-  return <App />
-}
+render(
+  <App />,
+  document.getElementById('root'),
+);
